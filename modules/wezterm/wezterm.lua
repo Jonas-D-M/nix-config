fe6e037377c449e7daeb3a518e791232ccf2262f
Jonas De Meyer <43569205+Jonas-D-M@users.noreply.github.com>
2025-11-03 21:12:59 +0100

    fix \ not working in wezterm

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -1,15 +1,23 @@
-local wezterm = require "wezterm"
-local config = wezterm.config_builder()
-local action = wezterm.action
+local wezterm                                      = require "wezterm"
+local config                                       = wezterm.config_builder()
+local action                                       = wezterm.action
 
-config.font = wezterm.font {
+config.font                                        = wezterm.font {
   family = 'JetBrains Mono',
   weight = 'Medium',
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, -- disable ligatures
 }
-config.font_size = 12.0
-config.line_height = 1.0
-config.window_padding = { left = '0.5cell', right = '0.5cell', top = '0.5cell', bottom = '0.5cell' }
-config.default_cursor_style = 'SteadyBlock'
+config.font_size                                   = 12.0
+config.line_height                                 = 1.0
+config.window_padding                              = {
+  left = '0.5cell',
+  right = '0.5cell',
+  top = '0.5cell',
+  bottom =
+  '0.5cell'
+}
+config.default_cursor_style                        = 'SteadyBlock'
+config.send_composed_key_when_left_alt_is_pressed  = true
+config.send_composed_key_when_right_alt_is_pressed = true
 
 return config
