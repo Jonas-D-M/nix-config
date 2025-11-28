247cd35cdcfdeae810cd230dbe54f68831791437
Jonas De Meyer <43569205+Jonas-D-M@users.noreply.github.com>
2025-11-28 16:14:24 +0100

    add proper keybindings

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -75,7 +75,7 @@
       load-nvmrc
 
       # Keymap & history
-      bindkey -e
+      # bindkey -e
       bindkey '^p' history-search-backward
       bindkey '^n' history-search-forward
 
@@ -94,6 +94,8 @@
       bindkey '^[[1;5D' backward-word
       bindkey '^H' backward-kill-word
       bindkey '^[[Z' undo
+      bindkey '^[[3~' delete-char
+      bindkey '^[[3;3~' kill-word
     '';
   };
 
