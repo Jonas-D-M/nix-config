a1a4f0c2a8792fb1154fab743d5c87e8d10601ef
Jonas De Meyer <43569205+Jonas-D-M@users.noreply.github.com>
2025-11-29 19:39:53 +0100

    fix wezterm config

--- modules/wezterm/wezterm.lua
+++ modules/wezterm/wezterm.lua
@@ -1,23 +1,33 @@
-local wezterm                                      = require "wezterm"
-local config                                       = wezterm.config_builder()
-local action                                       = wezterm.action
+local wezterm = require "wezterm"
 
-config.font                                        = wezterm.font {
-  family = 'JetBrains Mono',
-  weight = 'Medium',
-  -- harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }, -- disable ligatures
+local function scheme_for_appearance(appearance)
+  return appearance:find("Dark") and "Tokyo Night Storm" or "Tokyo Night Day"
+end
+
+local appearance = wezterm.gui.get_appearance()
+
+local config     = {
+  font                                        = wezterm.font {
+    family = "JetBrains Mono",
+    weight = "Medium",
+  },
+
+  font_size                                   = 14.0,
+  line_height                                 = 1.0,
+
+  window_padding                              = {
+    left   = "0.5cell",
+    right  = "0.5cell",
+    top    = "0.5cell",
+    bottom = "0.5cell",
+  },
+  window_decorations                          = "INTEGRATED_BUTTONS|RESIZE",
+
+  default_cursor_style                        = "SteadyBlock",
+  send_composed_key_when_left_alt_is_pressed  = true,
+  send_composed_key_when_right_alt_is_pressed = true,
+
+  color_scheme                                = scheme_for_appearance(appearance),
 }
-config.font_size                                   = 14.0
-config.line_height                                 = 1.0
-config.window_padding                              = {
-  left = '0.5cell',
-  right = '0.5cell',
-  top = '0.5cell',
-  bottom =
-  '0.5cell'
-}
-config.default_cursor_style                        = 'SteadyBlock'
-config.send_composed_key_when_left_alt_is_pressed  = true
-config.send_composed_key_when_right_alt_is_pressed = true
 
 return config
